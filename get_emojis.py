d466964820ab9530c7a230f01bf219d66255a71f
Jeff Kloosterman <Jeff.Kloosterman@dornerworks.com>
2014-08-28 16:24:17 -0400

    some housecleaning, added --trample to always download the images again.

--- get_emojis.py
+++ get_emojis.py
@@ -5,35 +5,40 @@ import argparse
 import requests
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 
 SLACK_URL = 'https://{slack}.slack.com/api/emoji.list?token={token}'
 
 
+def download_image(url, path):
+    r = requests.get(url, stream=True)
+    with open(path, 'wb') as f:
+        for chunk in r.iter_content():
+            f.write(chunk)
+
+
+def reconcile_aliases(image_url, emoji):
+    if image_url.startswith('alias:'):
+        image_url = emoji.get(image_url[6:], None)
+    return image_url
+
+
 def main(args):
     r = requests.get(SLACK_URL.format(slack=args.slack, token=args.token))
-    logging.debug(r.json())
-    logging.debug(r.status_code)
     if r.status_code == 200:
         emoji = r.json().get('emoji', {})
         if emoji and not os.path.isdir(args.output):
             os.mkdir(args.output)
         for image_name, image_url in emoji.items():
-            logging.debug(image_name)
-            logging.debug(image_url)
-            if image_url.startswith('alias:'):
-                image_url = emoji.get(image_url[6:])
-                if not image_url:
-                    continue
+            image_url = reconcile_aliases(image_url, emoji)
+            if not image_url:
+                continue
+            image_ext = image_url.split('.')[-1]
             image_path = os.path.join(args.output,
-                                      image_name + '.' +
-                                      image_url.split('.')[-1])
-            logging.debug(image_path)
-            r = requests.get(image_url, stream=True)
-            with open(image_path, 'wb') as f:
-                for chunk in r.iter_content():
-                    f.write(chunk)
+                                      image_name + '.' + image_ext)
+            if args.trample or not os.path.exists(image_path):
+                download_image(image_url, image_path)
 
 
 if __name__ == '__main__':
@@ -42,5 +47,8 @@ if __name__ == '__main__':
     parser.add_argument('token', help='auth token for slack.')
     parser.add_argument('-o', '--output', help='output location of images',
                         default=os.getcwd())
+    parser.add_argument('--trample',
+                        help='replace / redownload images even if one exists',
+                        action='store_true')
     args = parser.parse_args()
     main(args)
